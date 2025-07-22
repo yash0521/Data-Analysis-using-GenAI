48d6c123fd46e5d873c2bbe6e5ee32bc5c002e53
Yash Kathiriya <yashkathiriya@Yashs-MacBook-Air.local>
2025-07-22 13:11:57 +0200

    Remove unnecessory lines

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import numpy as np
 import glob
 import os
 
-# Set matplotlib to use non-interactive backend
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
--- run_server.py
+++ run_server.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import uvicorn
 import os
 from pathlib import Path
